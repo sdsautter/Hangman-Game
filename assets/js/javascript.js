ed0a459a9c39cd90c51bbc566153d4d5fc6bb01a
sdsautter <sdsautter@gmail.com>
2017-03-05 16:36:04 -0500

    Game should be in working order for the most part

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -101,6 +101,7 @@ var correctIndex = [];
 var boxArtImage = document.getElementById("boxArt");
 var alphabetBank = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "2", "3"]
 var gameStart = false;
+var gameOver = false;
 
 
 var displayIncorrectGuesses = document.getElementById('wrongGuesses');
@@ -111,42 +112,49 @@ var displayLosses = document.getElementById('losses');
 var displayChances = document.getElementById('chancesLeft');
 var displayBoxArt = document.getElementById('boxArt');
 var displayHungMario = document.getElementById('hungMario');
+var displayWinOrLose = document.getElementById('winOrLose');
+var displayPlayagain = document.getElementById('playAgain');
 
 
 function powerOff() {
-	displayPowerLight.innerHTML = "<img src='assets/images/power-off.png' alt='power n'>";
-	displayCorrectGuesses.innerHTML = "";
-	displayIncorrectGuesses.innerHTML = "";
-	displayWins.innerHTML = "";
-	displayLosses.innerHTML = "";
-	displayBoxArt.innerHTML = "<img src='assets/images/nes-classic-edition.png' alt='NES Classic'>";
-	displayChances.innerHTML = "";
-	displayHungMario.innerHTML = "";
-	gameStart = false;
-	gameWord = 0;
-	blanks = [];
-	correctGuesses = [];
-	incorrectGuesses = [];
-	correctIndex = [];
-	wins = 0;
-	losses = 0;
-	guessesLeft = 6;
+    displayPowerLight.innerHTML = "<img src='assets/images/power-off.png' alt='power n'>";
+    displayCorrectGuesses.innerHTML = "";
+    displayIncorrectGuesses.innerHTML = "";
+    displayWins.innerHTML = "";
+    displayLosses.innerHTML = "";
+    displayBoxArt.innerHTML = "<img src='assets/images/nes-classic-edition.png' alt='NES Classic'>";
+    displayChances.innerHTML = "";
+    displayHungMario.innerHTML = "";
+    displayPlayagain.innerHTML = "";
+    gameStart = false;
+    gameWord = 0;
+    blanks = [];
+    correctGuesses = [];
+    incorrectGuesses = [];
+    correctIndex = [];
+    wins = 0;
+    losses = 0;
+    guessesLeft = 6;
 
 }
 
 function newGame() {
-	gameWord = 0;
-	guessesLeft = 6;
-	blanks = [];
-	correctGuesses = [];
-	incorrectGuesses = [];
-	correctIndex = [];
+    gameWord = 0;
+    guessesLeft = 6;
+    gameOver = false;
+    blanks = [];
+    correctGuesses = [];
+    incorrectGuesses = [];
+    correctIndex = [];
     displayWins.innerHTML = wins;
     displayLosses.innerHTML = losses;
     displayChances.innerHTML = guessesLeft;
     gameWord = wordBank[Math.floor(Math.random() * wordBank.length)];
     console.log(gameWord);
     displayPowerLight.innerHTML = "<img src='assets/images/power-on.png' alt='power n'>";
+    displayWinOrLose.innerHTML = "";
+    displayPlayagain.innerHTML = "";
+    displayIncorrectGuesses.innerHTML = "";
     gameStart = true;
 
 
@@ -177,10 +185,10 @@ function newGame() {
 
 
 function guessCheck(UserGuess) {
-	UserGuess = UserGuess.toLowerCase();
+    UserGuess = UserGuess.toLowerCase();
     //This checks to see if the User Guess input was found in the array or not
-    if (gameWord.name.indexOf(UserGuess) >= 0 && correctGuesses.indexOf(UserGuess) < 0) {
-        
+    if (gameWord.name.toLowerCase().indexOf(UserGuess) >= 0 && correctGuesses.indexOf(UserGuess) < 0 && gameOver === false) {
+
         correctIndex = [];
         for (var i = 0; i < gameWord.name.length; i++) {
             if (gameWord.name[i].toLowerCase() === UserGuess) {
@@ -195,50 +203,64 @@ function guessCheck(UserGuess) {
         displayCorrectGuesses.textContent = blanks.join(" ");
 
 
-    } else if (gameWord.name.indexOf(UserGuess) < 0 && alphabetBank.indexOf(UserGuess) >= 0 && incorrectGuesses.indexOf(UserGuess) < 0) {
+    } else if (gameWord.name.indexOf(UserGuess) < 0 && alphabetBank.indexOf(UserGuess) >= 0 && incorrectGuesses.indexOf(UserGuess) < 0 && gameOver === false) {
         guessesLeft = guessesLeft - 1;
         chancesLeft.innerHTML = guessesLeft;
         document.getElementById("hungMario").innerHTML = "<img src='assets/images/" + guessesLeft + "-chances-left.png' alt='Hangman Progress'>";
         incorrectGuesses.push(UserGuess);
-            displayIncorrectGuesses.innerHTML = incorrectGuesses.join(" ");
-       }
-  
-	
+        displayIncorrectGuesses.innerHTML = incorrectGuesses.join(" ");
+    }
+
+
 
 }
 
 function gameProgress() {
 
-	console.log(guessesLeft + " This is in game progress function");
-	
-	if (blanks.indexOf("_") < 0 && guessesLeft > 0) {
-		alert("You Win");
-		wins++;
-		boxArtImage.innerHTML = gameWord.art;
-		console.log(guessesLeft + " This is if win");
-		newGame();
-	}
-	else if (guessesLeft <= 0 && blanks.indexOf("_") >= 0) {
-		losses++;
-		alert("You Lost");
-		newGame();
-	}
+    console.log(guessesLeft + " This is in game progress function");
+
+    if (blanks.indexOf("_") < 0 && guessesLeft > 0) {
+        displayWinOrLose.innerHTML = "You Won!";
+        displayPlayagain.innerHTML = "<p>Play Again</p>";
+        wins++;
+        boxArtImage.innerHTML = gameWord.art;
+    	correctGuesses = [];
+    	guessesLeft = 0;
+        gameOver = true;
+       
+        
+    } else if (guessesLeft <= 0 && blanks.indexOf("_") >= 0) {
+        losses++;
+        displayWinOrLose.innerHTML = "You Lose!";
+        displayHungMario.innerHTML = "<img src='assets/images/0-chances-left.png' alt='Hangman Progress'>";
+        displayPlayagain.innerHTML = "<p>Play Again</p>";
+        guessesLeft = 6;
+        gameOver = true;
+        	        
+    }
 }
 
+
+displayPlayagain.onclick = function() {
+    if (gameOver === true) {
+    	newGame();
+    }
+};
+
 document.getElementById("startBtn").onclick = function() {
-	if (gameStart === false) {
-		newGame();
-	} else if (gameStart === true){
-		powerOff();
-	}
+    if (gameStart === false) {
+        newGame();
+    } else if (gameStart === true) {
+        powerOff();
+    }
 
 };
 
 document.getElementById("resetBtn").onclick = function() {
-	if (gameStart === true) {
-		powerOff();
-		newGame();
-	} 
+    if (gameStart === true) {
+        powerOff();
+        newGame();
+    }
 };
 
 
